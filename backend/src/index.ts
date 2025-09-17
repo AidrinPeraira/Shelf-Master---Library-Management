73ba2b6da45b98ceac69f474b9f3e8f437a9b64e
Aidrin Peraira <aidrin.peraira@gmail.com>
2025-09-18 00:00:10 +0530

    backend server running

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,16 @@ import { GetBookByIdUseCase } from "./application/use-cases/Book/GetBookByIdUseC
 import { createBookRouter } from "./presentation/routes/bookRoutes.js";
 import { BookController } from "./presentation/controllers/BookController.js";
 import { MongoBookRepository } from "./infrastructure/repositories/MongoBookRepository.js";
+import { CreateUserUseCase } from "./application/use-cases/User/CreateUserUseCase.js";
+import { MongoUserRepository } from "./infrastructure/repositories/MongoUserRepository.js";
+import { LoginUserUseCase } from "./application/use-cases/User/LoginUserUseCase.js";
+import { UserController } from "./presentation/controllers/UserController.js";
+import { createUserRouter } from "./presentation/routes/userRoutes.js";
+import { MongoLoanRepository } from "./infrastructure/repositories/MongoLoanRepository.js";
+import { LendBookUseCase } from "./application/use-cases/Loan/LendBookUseCase.js";
+import { ReturnBookUseCase } from "./application/use-cases/Loan/ReturnBookUseCase.js";
+import { LoanController } from "./presentation/controllers/LoanController.js";
+import { createLoanRouter } from "./presentation/routes/loanRoutes.js";
 
 async function startServer() {
   dotenv.config();
@@ -22,14 +32,36 @@ async function startServer() {
 
   const db = await connectToDatabase();
 
+  //repositories
   const bookRepository = new MongoBookRepository(db);
+  const userRepository = new MongoUserRepository(db);
+  const loanRepository = new MongoLoanRepository(db);
 
+  //use cases
+  // //book
   const addBookUseCase = new AddBookUseCase(bookRepository);
   const deleteBookUseCase = new DeleteBookUseCase(bookRepository);
   const updateBookDetailsUseCase = new UpdateBookDetailsUseCase(bookRepository);
   const getAllBooksUseCase = new GetAllBooksUseCase(bookRepository);
   const getBookByIdUseCase = new GetBookByIdUseCase(bookRepository);
 
+  // //user
+  const createUserUseCase = new CreateUserUseCase(userRepository);
+  const loginUserUseCase = new LoginUserUseCase(userRepository);
+
+  // //loan
+  const lendBookUseCase = new LendBookUseCase(
+    loanRepository,
+    userRepository,
+    bookRepository
+  );
+  const returnBookUseCase = new ReturnBookUseCase(
+    bookRepository,
+    loanRepository
+  );
+
+  //controllers
+  // // book
   const bookController = new BookController(
     addBookUseCase,
     deleteBookUseCase,
@@ -37,10 +69,24 @@ async function startServer() {
     getBookByIdUseCase,
     updateBookDetailsUseCase
   );
+  // // user
+  const userControler = new UserController(createUserUseCase, loginUserUseCase);
 
+  // // loan
+  const loanController = new LoanController(lendBookUseCase, returnBookUseCase);
+
+  //routers
+  // //book
   const bookRouter = createBookRouter(bookController);
-
   app.use("/api/book", bookRouter);
+
+  // //user
+  const userRouter = createUserRouter(userControler);
+  app.use("/api/user", userRouter);
+
+  // //loan
+  const loanRouter = createLoanRouter(loanController);
+  app.use("/api/loan", loanRouter);
 
   const PORT = process.env.SERVER_PORT_NUMBER || 3000;
   app.listen(PORT, () => {
